209736b67a4059484a8f32b39d7bf76b17697589
jptyler <62032781+jptyler@users.noreply.github.com>
2021-08-24 10:37:41 -0400

    Update Final_results_fourier.m

--- Scripts/Final_results_fourier.m
+++ Scripts/Final_results_fourier.m
@@ -1,3 +1,8 @@
+% Copyright (c) 2021, Jonathan P. Tyler
+% All rights reserved.
+
+% This source code is licensed under the BSD-style license found in the
+% LICENSE file in the root directory of this source tree. 
 
 load('Output/regulation-detection-scores.mat')
 

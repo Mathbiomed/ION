0fcf54fe4be701bd3cb6b43d91396088664956f7
jptyler <62032781+jptyler@users.noreply.github.com>
2021-08-24 10:40:42 -0400

    Update main_linear.m

--- Scripts/main_linear.m
+++ Scripts/main_linear.m
@@ -1,4 +1,11 @@
 %%% Main Script %%%
+
+% Copyright (c) 2021, Jonathan P. Tyler
+% All rights reserved.
+
+% This source code is licensed under the BSD-style license found in the
+% LICENSE file in the root directory of this source tree. 
+
 % function [L1, L1g] = main
 
 clear rp_data e data

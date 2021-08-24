b7b0c61bc47d2f046b0cc35c63ed44a8323f7a0e
jptyler <62032781+jptyler@users.noreply.github.com>
2021-08-24 10:37:57 -0400

    Update compute_R.m

--- Scripts/compute_R.m
+++ Scripts/compute_R.m
@@ -1,3 +1,9 @@
+% Copyright (c) 2021, Jonathan P. Tyler
+% All rights reserved.
+
+% This source code is licensed under the BSD-style license found in the
+% LICENSE file in the root directory of this source tree. 
+
 function R = compute_R(data, rcolumn, scolumn, rvalue, t, rp_data, extrema)
 
 if(ismember(t, data(:,1)))

bec5374a5b9acd5b5da74c7a693a439a1fb15082
jptyler <62032781+jptyler@users.noreply.github.com>
2021-08-24 10:40:26 -0400

    Update main_fourier.m

--- Scripts/main_fourier.m
+++ Scripts/main_fourier.m
@@ -1,5 +1,10 @@
 %%% Main fourier method script %%%
 
+% Copyright (c) 2021, Jonathan P. Tyler
+% All rights reserved.
+
+% This source code is licensed under the BSD-style license found in the
+% LICENSE file in the root directory of this source tree. 
 
 % Load the regulation_detection_inputs
 
